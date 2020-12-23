5803de1f8a17da09253e5dfdb65942ccd8d54a71
iwom <17498068+iwom@users.noreply.github.com>
2020-12-23 18:48:26 +0100

    Add docker deps

---
+++ reservation/src/test/kotlin/com/iwom/theatre/reservation/ReservationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iwom.theatre.reservation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
