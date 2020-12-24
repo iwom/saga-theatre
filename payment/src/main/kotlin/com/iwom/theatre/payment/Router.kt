c29fd2458ee2a3e53f9b7d0ff8b7ff9f7e06d074
iwom <17498068+iwom@users.noreply.github.com>
2020-12-24 14:41:22 +0100

    init diagram

--- payment/src/main/kotlin/com/iwom/theatre/payment/Router.kt
+++ payment/src/main/kotlin/com/iwom/theatre/payment/Router.kt
@@ -1,26 +1,14 @@
 package com.iwom.theatre.payment
 
 import com.iwom.theatre.payment.event.ReservationPendingEvent
-import org.apache.camel.Exchange
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.model.dataformat.JsonLibrary
-import org.apache.camel.model.rest.RestBindingMode
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import javax.ws.rs.core.MediaType
 
 
 @Component
 class Router : RouteBuilder() {
-  @Value("\${server.port}")
-  lateinit var serverPort: String
-
-  @Value("\${payment.api.v1}")
-  lateinit var contextPath: String
-
   override fun configure() {
-
     from("kafka:reservation_events?brokers=localhost:9092")
       .routeId("processReservationCreated")
       .unmarshal()
