9934f8aca1a19af28f69f3ba888cda1ac581baad
iwom <17498068+iwom@users.noreply.github.com>
2021-01-09 18:23:23 +0100

    update stock

--- payment/src/main/kotlin/com/iwom/theatre/payment/repository/PaymentRepository.kt
+++ payment/src/main/kotlin/com/iwom/theatre/payment/repository/PaymentRepository.kt
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Service
 @Service
 class PaymentRepository {
   private final val paymentDetails = listOf(
-    PaymentDetails(userId = 1, name = "Josh Yellow", limit = 10, total = 0),
-    PaymentDetails(userId = 2, name = "George A.", limit = 10, total = 0),
-    PaymentDetails(userId = 3, name = "Mike Brown", limit = 50, total = 30)
+    PaymentDetails(userId = 1, name = "Josh Yellow", limit = 500, total = 0),
+    PaymentDetails(userId = 2, name = "George A.", limit = 1000, total = 0),
+    PaymentDetails(userId = 3, name = "Mike Brown", limit = 1200, total = 850),
+    PaymentDetails(userId = 4, name = "J. H. Blue", limit = 1000, total = 980),
   )
   private final val paymentDetailsByUserId = paymentDetails.associateBy { it.userId }
 
--- reservation/src/main/kotlin/com/iwom/theatre/reservation/service/MovieService.kt
+++ reservation/src/main/kotlin/com/iwom/theatre/reservation/service/MovieService.kt
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Service
 @Service
 class MovieService {
   private final val movies: List<Movie> = listOf(
-    Movie(id = 1, name = "Seven", year = 1995, price = 5, seats = 10),
-    Movie(id = 2, name = "Interstellar", year = 2014, price = 2, seats = 50),
-    Movie(id = 3, name = "Stalker", year = 1979, price = 3, seats = 20)
+    Movie(id = 1, name = "Seven", year = 1995, price = 20, seats = 150),
+    Movie(id = 2, name = "Interstellar", year = 2014, price = 30, seats = 50),
+    Movie(id = 3, name = "Stalker", year = 1979, price = 10, seats = 20),
+    Movie(id = 4, name = "Toy Story", year = 1995, price = 10, seats = 100),
+    Movie(id = 5, name = "The Turin Horse", year = 2011, price = 10, seats = 30),
+    Movie(id = 6, name = "Mision: Impossible 7", year = 2021, price = 100, seats = 150),
   )
 
   val moviesById = movies.associateBy { it.id }
